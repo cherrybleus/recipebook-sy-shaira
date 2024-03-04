d37c17c3b0c33b3eed0aac2cf4df418ed058027c
Shaira Sy <shaira.sy@student.ateneo.edu>
2024-03-04 12:27:07 +0800

    Finished Lab2

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient, Recipe, RecipeIngredient
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+    list_display = ['recipe', 'quantity', 'ingredient',]
+    list_filter = ['recipe',]
+
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                'recipe', ('quantity', 'ingredient')
+            ]
+        })
+    ]
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInline,]
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
--- ledger/urls.py
+++ ledger/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from .views import index, recipes, recipe_1, recipe_2
+from .views import RecipeDetailView, RecipesListView
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('recipes/list', recipes, name='list'),
-    path('recipe/1', recipe_1, name='1'),
-    path('recipe/2', recipe_2, name='2')
+    path('recipes/list', RecipesListView.as_view(), name='list'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe'),
 ]
 
 app_name = 'ledger'
